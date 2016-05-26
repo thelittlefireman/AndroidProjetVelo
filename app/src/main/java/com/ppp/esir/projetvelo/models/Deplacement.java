ba8d732fa1f18e3e6febf421c181d7cd09c4b202
thelittlefireman <thomas.goureau@gmail.com>
2016-05-26 16:35:49 +0200

    deplacement

--- app/src/main/java/com/ppp/esir/projetvelo/models/Deplacement.java
+++ app/src/main/java/com/ppp/esir/projetvelo/models/Deplacement.java
@@ -4,11 +4,10 @@ package com.ppp.esir.projetvelo.models;
  * Created by thoma on 18/05/2016.
  */
 public class Deplacement {
-    private String vitesse, deplacement, depart, arrivee, distance;
+    private String vitesse, depart, arrivee, distance;
 
-    public Deplacement(String vitesse, String deplacement, String depart, String arrivee, String distance) {
+    public Deplacement(String vitesse, String depart, String arrivee, String distance) {
         this.vitesse = vitesse;
-        this.deplacement = deplacement;
         this.depart = depart;
         this.arrivee = arrivee;
         this.distance = distance;
@@ -22,9 +21,6 @@ public class Deplacement {
         return distance;
     }
 
-    public String getDeplacement() {
-        return deplacement;
-    }
 
     public String getDepart() {
         return depart;
